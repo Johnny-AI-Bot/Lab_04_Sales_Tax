306ff9909f09ce13abd144f6a9b5302830a81237
jatur <turayjj@mail.uc.edu>
2024-09-19 15:52:14 -0400

    Init Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double purchasePrice = 99.22;
+        double salesTax = 4.961;
+        final double TAX_RATE = .05;
+
+        salesTax = purchasePrice * TAX_RATE;
+
+        System.out.println("The sales tax on a purchase of" + purchasePrice + " is " + salesTax);
+
     }
 }
